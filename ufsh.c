0807783f9b831918e7977bdad579763eaa0e9f4b
James Ananenkovic <152923178+JamesAnanenkovic@users.noreply.github.com>
2025-01-04 23:06:42 +0300

    Update ufsh.c

--- ufsh.c
+++ ufsh.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+
+	char komut[32] = "...";
+
+	printf("Uf Shell started\n");
+	scanf("%s",komut);
+
+	if (komut == "help");
+		printf("Ufinished Shell v0.01\nPlease Upgrade\n");
+
+// sıradaki şeyler
+
+// kullanıcı ismi eklenecek
+}
